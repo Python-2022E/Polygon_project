967facfaab505a08e95797bdf1442e375bb3da22
Akbar Abdullayev <abdullayevakbar0101@gmail.com>
2022-10-06 15:53:11 +0500

    triangle

--- triangle.py
+++ triangle.py
@@ -8,7 +8,7 @@ class Triangle:
         self.c = c
 
     def is_valid(self) -> bool:
-        return (self.a > 0 and self.b and self.c and max(self.a, self.c, self))
+        return (self.a > 0 and self.b > 0 and self.c > 0 and max(self.a, self.c, self)*2 < self.a+self.b+self.c)
 
     def get_type(self) -> str:
         pass

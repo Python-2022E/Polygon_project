6733907c43e931a5bb005afc823ee6dc6ea7a72a
sodiqoydinov <sodiq7898@gmail.com>
2022-10-08 13:36:25 +0500

    circle01

--- circle.py
+++ circle.py
@@ -4,15 +4,18 @@ class Circle:
         self.radius = radius
         
     def is_valid(self) -> bool:
-        return self.radius > 0
+        return self.radius>0
     
     def diameter(self):
-        return self.radius *2
+        return 2 * self.radius
     
     def circumference(self) -> float:
         return 2*pi*self.radius
     
     def area(self) -> float:
-        return pi*self.radius**2
-x = Circle(5)
-print(x.diameter())
+        return pi*(self.radius)**2
+circle = Circle(4)
+is_valid_circle = circle.is_valid()
+circle_diameter = circle.diameter()
+circle_circumference = circle.circumference()
+circle_area = circle.area()
